d132327dde3b76e826676dae59c8ddd1b9130894
kmchmk1026 <kmchmk@gmail.com>
2017-08-19 00:27:04 +0530

    Guiding steps are added. (For Huawei phones)

--- AppVersion/version.php
+++ AppVersion/version.php
@@ -1,12 +1,12 @@
 <?php
     $jsonObj = new stdClass();
-    $jsonObj->newversion = 1;
+    $jsonObj->newversion = 2;
     $jsonObj->title = "New Update";
-    $jsonObj->message = "A new update (1.76MB) is available for this application.\n * Performance improved.\n* Bugs fixed.\n* (Background login is not supported for Huawei yet)";//\n(Use another browser other than Chrome)";
-    $jsonObj->positivebutton = "Download";
+    $jsonObj->message = "A new update (2.47MB) is available for this application.\n * Performance improved.\n* Bugs fixed.\n* Background login is now supported for Huawei devices.\n* (If nothing happens when you click \"DOWNLOAD\", contact me)\n* (Use another browser other than Chrome)";
+    $jsonObj->positivebutton = "DOWNLOAD";
     $jsonObj->negativebutton = "Cancel";
-    $jsonObj->apkurl = "https://www.dropbox.com/sh/bmmfxrx2yfpjf59/AACIXoJ7_RnSnmTeoZEpkEeDa?dl=0";
-    // $jsonObj->apkurl = "http://13.58.202.127/UoM_Wireless_App/UoM_Wireless.apk";
+    // $jsonObj->apkurl = "https://www.dropbox.com/sh/bmmfxrx2yfpjf59/AACIXoJ7_RnSnmTeoZEpkEeDa?dl=0";
+    $jsonObj->apkurl = "http://13.58.202.127/UoM_Wireless_App/UoM_Wireless.apk";
     echo json_encode($jsonObj);
 
 
