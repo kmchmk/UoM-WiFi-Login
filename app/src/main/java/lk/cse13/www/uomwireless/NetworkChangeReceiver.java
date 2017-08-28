fa7b3663d9ce3874e005b45ee9da9fbe5e26e580
kmchmk1026 <kmchmk@gmail.com>
2017-08-29 01:21:41 +0530

    Bug fixes. image changed.

--- app/src/main/java/lk/cse13/www/uomwireless/NetworkChangeReceiver.java
+++ app/src/main/java/lk/cse13/www/uomwireless/NetworkChangeReceiver.java
@@ -8,12 +8,11 @@ import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 
 public class NetworkChangeReceiver extends BroadcastReceiver {
-
     @Override
     public void onReceive(Context context, Intent intent) {
+        MainActivity.mainContext = context;
         NetworkInfo netinfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
         if (netinfo != null && netinfo.isConnected()) {
-            MainActivity.mainContext = context;
             new BackgroundLogin(0).execute();
         } else {
             Operations.cancelNotification();
