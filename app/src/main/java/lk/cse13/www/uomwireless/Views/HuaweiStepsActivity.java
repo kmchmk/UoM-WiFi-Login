478de62c840d814deea456d92090feaa5eddf894
kmchmk1026 <kmchmk@gmail.com>
2018-01-23 23:15:00 +0530

    Action bar is added Huawei Steps.

--- app/src/main/java/lk/cse13/www/uomwireless/Views/HuaweiStepsActivity.java
+++ app/src/main/java/lk/cse13/www/uomwireless/Views/HuaweiStepsActivity.java
@@ -2,7 +2,9 @@ package lk.cse13.www.uomwireless.Views;
 
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import lk.cse13.www.uomwireless.R;
 
@@ -12,5 +14,24 @@ public class HuaweiStepsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_huaweisteps);
+        setupActionBar();
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
